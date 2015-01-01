4461526e0f7bbeb5329ac2af0efe1fad05a0fbe2
Christian Guedemann <christian.guedemann@webgate.biz>
2015-01-01 16:56:26 +0100

    Base Infrastucture

---
+++ ch.christianguedemann.demoplugin/ch.christianguedemann.demoplugin.angular/src/ch/christianguedemann/demoplugin/angular/library/AJSLibrary.java
@@ -0,0 +1,15 @@
+package ch.christianguedemann.demoplugin.angular.library;
+
+import com.ibm.xsp.library.AbstractXspLibrary;
+
+public class AJSLibrary extends AbstractXspLibrary {
+
+	public AJSLibrary() {
+	}
+
+	@Override
+	public String getLibraryId() {
+		return "ch.christianguedemann.demoplugin.angular.library";
+	}
+
+}
---
+++ ch.christianguedemann.demoplugin/ch.christianguedemann.demoplugin.angular/src/ch/christianguedemann/demoplugin/angular/resources/AJSResourceProvider.java
@@ -0,0 +1,46 @@
+package ch.christianguedemann.demoplugin.angular.resources;
+
+import java.net.URL;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.framework.Bundle;
+
+import ch.christianguedemann.demoplugin.angular.AJSActivator;
+
+import com.ibm.xsp.resource.ScriptResource;
+import com.ibm.xsp.webapp.resources.BundleResourceProvider;
+
+public class AJSResourceProvider extends BundleResourceProvider {
+
+	public static final String RESOURCES_WEB_ANGULAR = "/resources/web/angular/";
+	public static final String ANGULAR_PREFIX = ".angular";
+
+	public static ScriptResource ANGULAR_1_1_4 = new ScriptResource("/.ibmxspres/"+ANGULAR_PREFIX +"/angular.1.1.4.min.js",true);
+	public static ScriptResource ANGULAR_1_2_28 = new ScriptResource("/.ibmxspres/"+ANGULAR_PREFIX +"/angular.1.2.28.min.js",true);
+	
+	
+	public AJSResourceProvider(Bundle arg0, String arg1) {
+
+		super(AJSActivator.getInstance().getBundle(), ANGULAR_PREFIX);
+	}
+
+	@Override
+	protected URL getResourceURL(HttpServletRequest arg0, String name) {
+		String path = RESOURCES_WEB_ANGULAR + name; // $NON-NLS-1$
+		int fileNameIndex = path.lastIndexOf('/');
+		String fileName = path.substring(fileNameIndex + 1);
+		path = path.substring(0, fileNameIndex + 1);
+		// see http://www.osgi.org/javadoc/r4v42/org/osgi/framework/Bundle.html
+		// #findEntries%28java.lang.String,%20java.lang.String,%20boolean%29
+		Enumeration<?> urls = getBundle().findEntries(path, fileName, false/* recursive */);
+		if (null != urls && urls.hasMoreElements()) {
+			URL url = (URL) urls.nextElement();
+			if (null != url) {
+				return url;
+			}
+		}
+		return null; // no match, 404 not found.
+	}
+}
